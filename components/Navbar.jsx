0b767eda0129797efccf708245bf753c3d4cc8e7
Kevin Phillips <knphillips01@gmail.com>
2022-04-30 15:59:26 -0500

    committing search page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,7 +14,7 @@ import { BsSearch } from 'react-icons/bs';
 import { FiKey } from 'react-icons/fi';
 
 const Navbar = () => (
-  <Flex p='2' borderBottom='1px' borderColo='gray.100'>
+  <Flex p='2' borderBottom='1px' borderColor='gray.100'>
     <Box fontSize='3xl' color='blue.400' fontWeight='bold'>
       <Link href='/' paddingLeft='2'>
         Phillips Realty
@@ -26,7 +26,7 @@ const Navbar = () => (
         <MenuButton
           as={IconButton}
           icon={<FcMenu />}
-          variant='outlined'
+          variant='outline'
           color='red.400'
         />
         <MenuList>
